d595c5d3acd018e69ae5fd2279e19b8bd31b1d00
abdalaMask2022 <abdalaMask2022@ABDALAMASK2022>
2022-01-12 18:22:16 +0200

    Add Cylinder

---
+++ Controls/HslStudio.HslControls.Feed/Cylinder.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+
+namespace HslStudio.HslControls.Feed
+{
+    public  class Cylinder:Control
+    {
+        private bool _isAlarmOn;
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            _isAlarmOn = !_isAlarmOn; // toggle alarm
+            PseudoClasses.Set(":alarm", _isAlarmOn);
+        }
+    }
+}
