857807f6d02a51f6e5fae053ae78c2140624e429
abdalaMask2022 <abdalaMask2022@ABDALAMASK2022>
2022-01-13 16:05:09 +0200

    Add LedSingle tow

--- Controls/HslStudio.HslControls/LedSingle.cs
+++ Controls/HslStudio.HslControls/LedSingle.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +17,27 @@ namespace HslStudio.HslControls
     }
     public class LedSingle : ToggleButtonBasic
     {
+        public static readonly StyledProperty<LedStyle> LedStyleProperty =
+             AvaloniaProperty.Register<LedSingle, LedStyle>(nameof(LedStyle), LedStyle.Circle);
 
+        [Category("HMI")]
+        public LedStyle LedStyle
+        {
+            get => (LedStyle)GetValue(LedStyleProperty);
+            set => SetValue(LedStyleProperty, value);
+        }
+        private bool _isAlarmOn;
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            base.OnPointerPressed(e);
+
+            _isAlarmOn = !_isAlarmOn; // toggle alarm
+            PseudoClasses.Set(":alarm", _isAlarmOn);
+        }
+        public LedSingle()
+        {
+            //PseudoClasses.Set(":Circle", true);
+            //PseudoClasses.Set(":Rectangle", false);
+        }
     }
 }
